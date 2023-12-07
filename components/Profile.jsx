6e8c7a6196ede07a453bea927c536e946036e49e
Abusantos <abusomwansantos@gmail.com>
2023-12-07 04:56:02 +0100

    updating the profile

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Profile = () => {
   return (
     <>
-      <div className="flex items-center space-x-4 mt-28 ">
+      <div className="flex items-center space-x-4 mt-16 ">
         <div>
           <Image src={profilepicture} alt="profile-image" className="h-44 " />
         </div>
